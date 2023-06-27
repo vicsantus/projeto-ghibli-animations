95ccc984666fd282b12fdc76809ffeef85a64b40
victrosantos <victor.santos.fk@hotmail.com>
2023-06-27 11:52:14 -0300

    fix: Corrigido Frontend

--- src/database/routers/index.ts
+++ src/database/routers/index.ts
@@ -7,5 +7,6 @@ const router = Router();
 const filmsController = new FilmsController();
 
 router.get('/', filmsController.getAll);
+router.get('/images/:url', filmsController.getImage);
 
 export default router;
