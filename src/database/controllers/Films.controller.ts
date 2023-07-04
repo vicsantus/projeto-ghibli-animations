2462d1c710dd63c815c7d52bd941592e99d46c76
victrosantos <victor.santos.fk@hotmail.com>
2023-07-04 17:41:37 -0300

    feat: Feito mocks e teste da rota de backend

--- src/database/controllers/Films.controller.ts
+++ src/database/controllers/Films.controller.ts
@@ -3,12 +3,12 @@ import path from 'path';
 import FilmsModel from '../models/Films.model';
 
 class FilmsController {
-  async getAll(_req: Request, res: Response) {
+  async getAll(_req: Request, res: Response):Promise<any> {
     const films = await FilmsModel.findAll();
     return res.status(200).json(films);
   };
 
-  async getImage(req:Request, res:Response):Promise<void> {
+  async getImage(req:Request, res:Response):Promise<any> {
     const { url } = req.params;
     return res.sendFile(path.resolve(`imgs/${url}`));
   }
--- src/tests/Films.test.ts
+++ src/tests/Films.test.ts
@@ -1,5 +1,7 @@
 import * as chai from 'chai';
+import fs from 'fs';
 import * as sinon from 'sinon';
+import request from "supertest";
 
 // @ts-ignore
 import chaiHttp = require('chai-http');
@@ -8,10 +10,11 @@ import { app } from '../app';
 import FilmsController from '../database/controllers/Films.controller';
 import FilmsModel from '../database/models/Films.model';
 
-import { Response } from 'superagent';
 
+import { Response } from 'express';
 import { Model } from 'sequelize';
 import { getAll } from './mocks/getAll.mock';
+const kikis = fs.readFileSync(`./imgs/kikis.jpg`);
 
 const sandbox = sinon.createSandbox();
 
@@ -19,133 +22,36 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Verificando rota de users', () => {
+describe('Verificando rota geral', () => {
   afterEach(function () {
     sinon.restore();
     sandbox.restore();
   });
 
-  // -------------------Email-------------------
-  describe('listagem de email em service users', function () {
-    it('retorna o email de users', async function () {
+  // -------------------Rotas-------------------
+  describe('Retorna o valor correto na rota: ', function () {
+    it('/', async function () {
       // arrange
       sinon.stub(Model, 'findAll').resolves(getAll as FilmsModel[]);
 
       // act
-      const result = await FilmsController.getAll(Request, Response) as Response;
+      const result = await request(app).get("/") as unknown as Response & {body: any, statusCode: any};
 
       // assert
-      expect(result).to.deep.equal(getAll);
+      expect(result.statusCode).to.deep.equal(200);
+      expect(result.body).to.deep.equal(getAll)
     });
 
-    it('retorna mensagem quando coloca email errado em users', async function () {
+    it('/images/:url', async function () {
       // arrange
-      sinon.stub(Model, 'findOne').resolves(null);
+      sinon.stub(FilmsController.prototype, 'getImage').returns(Promise.resolve(kikis));
 
       // act
-      const result = await FilmsController.getByEmail('666');
+      const result = await request(app).get("/images/kikis.jpg") as unknown as Response & {body: any, statusCode: any};
 
       // assert
-      expect(result).to.deep.equal({ message: 'Email não encontrado' });
-    });
-  });
-
-  // ---------------Role-----------------
-  // describe('listagem de role em service users', function () {
-  //   it('retorna o role e status de user expecifico', async function () {
-  //     // arrange
-  //     sandbox.stub(Jwt, 'verify').returns({ data: userAdmin });
-  //     sinon.stub(Model, 'findOne').resolves(userAdmin as UserModel);
-
-  //     // act
-  //     const result = await UserService.role(userAdmin.email) as {type: number, message: string};
-
-  //     // assert
-  //     expect(result.type).to.deep.equal(200);
-  //     expect(result.message).to.deep.equal(userAdmin.role);
-  //   });
-  // });
-
-  // ---------------Login-----------------
-  describe('listagem de login em service users', function () {
-    it('retorna o login e status de user expecifico', async function () {
-      // UserModel.findOne
-      // bcrypt.compareSync
-      // UserService.generateToken
-      
-      // arrange
-      sinon.stub(Model, 'findOne').resolves(getAll as FilmsModel);
-      sinon.stub(FilmsController, 'generateToken').resolves('QWERTQWERTQWERTtokenEnormeQWERTQWERTQWERT');
-
-      // act
-      const result = await FilmsController.login(getAll) as {type: number, message: object};
-      const msg = await result.message as {token: string};
-
-      // assert
-      expect(result.type).to.deep.equal(200);
-      expect(await msg.token).to.deep.equal('QWERTQWERTQWERTtokenEnormeQWERTQWERTQWERT');
-    });
-
-    it('retorna status e mensagem de erro com password errado de user', async function () {
-      // UserModel.findOne
-      // bcrypt.compareSync
-      // UserService.generateToken
-      
-      // arrange
-      sinon.stub(Model, 'findOne').resolves(getAll as FilmsModel);
-
-      // act
-      const result = await FilmsController.login(getAll) as {type: number, message: object};
-
-      // assert
-      expect(result.type).to.deep.equal(401);
-      expect(result.message).to.deep.equal({ message: 'Invalid email or password' });
-    });
-
-    it('retorna status e mensagem de erro com email errado de user', async function () {
-      // UserModel.findOne
-      // bcrypt.compareSync
-      // UserService.generateToken
-      
-      // arrange
-      sinon.stub(Model, 'findOne').resolves(null);
-
-      // act
-      const result = await FilmsController.login(getAll) as {type: number, message: object};
-
-      // assert
-      expect(result.type).to.deep.equal(401);
-      expect(result.message).to.deep.equal({ message: 'Invalid email or password' });
-    });
-  });
-
-  describe('verifica controller de users', function () {
-    it('retorna token de login', async function () {
-      // arrange
-      // sinon.stub(UserService, 'getAll').resolves(userAdmin);
-
-      // act
-      const httpResponse = await chai
-        .request(app)
-        .post('/login')
-        .send({
-          email: getAll.email,
-          password: 'secret_admin'
-        })
-      const {token} = httpResponse.body;
-
-      // assert
-      expect(httpResponse.status).to.equal(200);
-      expect(httpResponse.body).to.be.deep.equal({token});
-
-      const httpRespRole = await chai
-      .request(app)
-      .get('/login/role')
-      .auth(token, 'pass')
-      .set('Authorization', `${token}`);
-
-      expect(httpRespRole.status).to.equal(401);
-      expect(httpRespRole.body).to.be.deep.equal({message: "Token must be a valid token"});
+      expect(result.statusCode).to.deep.equal(200);
+      expect(result.body).to.deep.equal(kikis)
     });
   });
 });
